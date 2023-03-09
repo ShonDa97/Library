90f736573bfdd5700b289cdea03c7d76adb3f54b
Jhon <jdcastaneda52@gmail.com>
2023-03-09 00:31:48 -0500

    favicon, types and constants

--- frontend/src/types.d.ts
+++ frontend/src/types.d.ts
@@ -1,5 +1,18 @@
 export interface IconProps {
-  width?: number
-  height?: number
+  width: number
+  height: number
   strokeWidth: number
 }
+
+export interface Book {
+  id: string
+  author: string
+  country: string
+  imageLink: string
+  language: Language
+  title: string
+  year: number
+  lend: boolean
+}
+
+export type ListOfBooks = Book[]
