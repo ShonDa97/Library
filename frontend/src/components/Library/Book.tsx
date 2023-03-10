38ed449fe78215a6289fb8365264e77988322451
Jhon <jdcastaneda52@gmail.com>
2023-03-09 22:30:00 -0500

    abort controller

--- frontend/src/components/Library/Book.tsx
+++ frontend/src/components/Library/Book.tsx
@@ -4,19 +4,21 @@ interface Props {
   book: BookType
 }
 
-export const Book: React.FC<Props> = ({ book: { author, country, imageLink, language, title, year, lend } }) => {
+export const Book: React.FC<Props> = ({
+  book: { author, country, imageLink, language, title, year, lend }
+}) => {
   return (
     <li className={`book-card ${lend ? 'borrow' : ''}`}>
-      <div className="book-card-header">
+      <div className='book-card-header'>
         <img src={imageLink} alt={title} height='450' />
-        <div className="badges">
-          {lend && (<span>BORROWED</span>)}
+        <div className='badges'>
+          {lend && <span>BORROWED</span>}
 
           <span>{year}</span>
           <span>{language}</span>
         </div>
       </div>
-      <div className="book-card-footer">
+      <div className='book-card-footer'>
         <p className='author'>{author}</p>
         <p className='country'>{country}</p>
       </div>
--- frontend/src/components/Library/index.tsx
+++ frontend/src/components/Library/index.tsx
@@ -4,19 +4,22 @@ import { ListOfBooks } from './ListOfBooks'
 
 export const Library = (): JSX.Element => {
   const [books, setBooks] = useState([])
+
   useEffect(() => {
-    fetch(`${URL_BACKEND}/books`)
+    const controller = new AbortController()
+    const { signal } = controller
+    fetch(`${URL_BACKEND}/books`, { signal })
       .then(async (res) => await res.json())
       .then((json) => {
         setBooks(json)
       })
       .catch((e) => {
-        console.log(e)
+        if (e.message !== 'AbortError') {
+          console.log(e)
+        }
       })
 
-    // return () => {
-    //   second
-    // }
+    return () => { controller.abort() }
   }, [])
 
   return (
